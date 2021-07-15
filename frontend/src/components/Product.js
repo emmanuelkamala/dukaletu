43a3f917f89df3e2d6cedc7f812dcf0109ecdd86
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-15 09:36:25 +0300

    Edit functionality in App.js file

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 function Product(props) {
@@ -7,19 +8,19 @@ function Product(props) {
 
   return (
     <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
-                <div className="price">
-                  ${product.price}
-                </div>
-              </div>
-            </div>
+      <Link to={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
+      </Link>
+      <div className="card-body">
+        <Link to={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+        <div className="price">
+          ${product.price}
+        </div>
+      </div>
+    </div>
   )
 }
 
