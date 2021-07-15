ef3d086b52d190d084b21e99cb5d3ebc2bd1a9a4
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-15 19:01:22 +0300

    add proceed to checkout section

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -21,6 +21,10 @@ function CartScreen(props) {
 
   }
 
+  const checkoutHandler = (id) => {
+
+  }
+
   return (
     <div className="row top">
       <div className="col-2">
@@ -41,7 +45,7 @@ function CartScreen(props) {
                         <Link to={`/product/${item.product}`}>{item.name}</Link>
                       </div>
                       <div>
-                        <select value={item.qty} onChange={e => dispatch(addToCart(item.product), Number(e.target.value))}>
+                        <select value={item.qty} onChange={e => dispatch(addToCart(item.product, Number(e.target.value)))}>
                           {[...Array(item.countInStock).keys()].map(
                             (x) => (
                               <option key={x + 1} value={x + 1}>
@@ -66,6 +70,24 @@ function CartScreen(props) {
             </ul>
           )
         }
+      </div>
+      <div className="col-1 space">
+        <div className="card card-body">
+          <ul>
+            <li>
+              <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : 
+                 {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}/=
+              </h2>
+            </li>
+            <li>
+              <button type="button" onClick={checkoutHandler} className="primary block" disabled={cartItems.length === 0}>
+                Proceed to Checkout
+              </button>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   )
