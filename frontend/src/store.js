466d31c4b1190a15bcb7f700092a8e9374bda788
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-16 12:24:20 +0300

    Add place order screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,8 +14,8 @@ const initialState = {
   cart: {
     cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
     shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {},
+    paymentMethod: 'Beem Pay',
   },
-  paymentMethod: 'Beem Pay',
 };
 const reducer = combineReducers({
   productList: productListReducer,
