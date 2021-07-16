02fcc82214ee8869a144a5125da31f88b4c39150
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-16 17:11:39 +0300

    Add place order screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { ORDER_CREATE_RESET } from '../constants/orderConstants';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = (props) => {
   const cart = useSelector(state => state.cart);
   // if (!cart.paymentMethod){
   //   props.history.push('/payment');
   // }
 
+  const orderCreate = useSelector((state => state.orderCreate));
+  const { loading, success, error, order } = orderCreate;
   const toPrice = num => Number(num);
   cart.itemsPrice = toPrice(cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0));
   cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10)
   cart.taxPrice = toPrice(0.18 * cart.itemsPrice);
   cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+  const dispatch = useDispatch();
   const placeOrderHandler = () => {
-
+    dispatch(createOrder({...cart, orderItems: cart.cartItems}))
   }
 
+  useEffect(()=>{
+    if (success){
+      props.history.push(`/order/${order._id}`);
+      dispatch({ type: ORDER_CREATE_RESET});
+    }
+  },[dispatch, success, props.history, order])
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
@@ -107,6 +120,8 @@ const PlaceOrderScreen = () => {
                     disabled={cart.cartItems.length === 0}
                   >Place Order</button>
               </li>
+              { loading && <LoadingBox></LoadingBox> }
+              { error && <MessageBox variant='danger'>{error}</MessageBox>}
             </ul>
           </div>
         </div>
